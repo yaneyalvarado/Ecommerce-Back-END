787af929db59218df447ecfb0566acfaca3c87c3
yaneyalvarado <alvyaney@gmail.com>
2023-05-22 10:49:06 -0600

    added code in product.js, productTag.js, tag.js, category.js, and category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,85 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+    Category.findAll ({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id' ]
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  if(error) throw error;
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+   Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id' ]
+      }
+    ]
+  })
+   .then(dbCategoryData => {
+    if (dbCategoryData) {
+      res.status().json({ message: 'Category not found' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+    if(error) throw error;
 });
 
 router.post('/', (req, res) => {
   // create a new category
-});
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    if(error) throw error;
+  });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (dbCategoryData) {
+      res.status().json({ message: 'Category not found' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  if(error) throw error;
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (dbCategoryData) {
+      res.status().json({ message: 'Category not found' });
+      return;
+    } 
+     res.json(dbCategoryData);
+  })
+  if(error) throw error;
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -18,3 +18,5 @@
 - THEN the data for each of these routes is displayed in a formatted JSON
 - WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
 - THEN I am able to successfully create, update, and delete data in my database
+
+## GitHub
