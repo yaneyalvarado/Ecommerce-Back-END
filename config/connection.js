729cba2115c56f76b4c9862f31f7fce2faa392c4
yaneyalvarado <alvyaney@gmail.com>
2023-07-01 08:31:11 -0600

    ensuring it's up to date

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize('ecommerce_db', 'root', 'yourpassword', {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
@@ -13,3 +13,14 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// module.exports = sequelize;
